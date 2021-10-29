50978f2c3a7202e4d5bc89e677dae4f8d5c110b4
yanghong <hong.yang@qizhixinxi.com>
2021-10-29 10:44:49 +0800

    增加报告显示demo

--- run_test.py
+++ run_test.py
@@ -7,7 +7,11 @@
 @desc:
 '''
 import pytest
+from common_utils.appium_serve import AppiumServe
 import os
 if __name__ == '__main__':
     pytest.main(['-v','--alluredir=./allure-report/raw','./TestCases/test_demo.py'])
     os.system("allure generate ./allure-report/raw -o allure-report/html --clean")
+    #查看报告
+    # AppiumServe.release_port('8088')
+    # os.system("allure open -h 127.0.0.1 -p 8088 ./allure-report/html")
